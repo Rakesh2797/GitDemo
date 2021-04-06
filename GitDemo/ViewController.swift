c0cb7fb59f578d40c15e6868326f84712a14aeb7
Rakesh <sairakesh.kota@gmail.com>
2021-04-06 16:17:28 +0530

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Kota, Venkata | RIEPL on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
